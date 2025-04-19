5bf292443ee6453d05fab26f8e1e6b0489548ab8
RitheshJgit <ritheshjayapal@gmail.com>
2025-04-19 13:43:19 +0530

    updated medium question

---
+++ Contest/randomArrays.java
@@ -0,0 +1,17 @@
+package Contest;
+
+public class randomArrays {
+    public static void main(String[] args) {
+        int[] arr ={
+                13, 16, 22, 31, 46, 64, 85, 109, 139, 172, 208, 247, 289, 337, 388, 442, 499, 559,
+                622, 691, 763, 838, 916, 997, 1081, 1168, 1261, 1357, 1456, 1558, 1663, 1771, 1882,
+                1996, 2116
+        };
+
+        findRandom(arr);
+    }
+
+    public static void findRandom(int[] arr){
+        System.out.println(arr.length);
+    }
+}
